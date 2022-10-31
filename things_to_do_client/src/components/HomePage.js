be531a83f5c939578f7d9d40ac037f1776ab39aa
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2022-10-31 13:07:58 +0000

    added logic to render todos onto /list page from database

--- things_to_do_client/src/components/HomePage.js
+++ things_to_do_client/src/components/HomePage.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const HomePage = () => {
+  const navigate = useNavigate();
+
+  const navigateToTodos = () => {
+    navigate("/list");
+  };
+   const navigateToAddTodo = () => {
+     navigate("/new");
+   };
+
   return (
     <div>
       <h1>Things to do List!</h1>
       <h3>What would you like to do today?</h3>
-      <button>Check my list</button>
-      <button>Add Something to my list</button>
+      <button onClick={navigateToTodos}>Check my list</button>
+      <button onClick={navigateToAddTodo}>Add Something to my list</button>
     </div>
-  )
+  );
 }
