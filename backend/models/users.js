f929129611f903c3778c6a1c89f1bb57391bf688
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2022-10-31 13:33:59 +0000

    added bcrypt to backend user schema

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,9 +1,33 @@
 const mongoose= require('mongoose');
+import bcrypt from 'bcrypt';
 
 const UserSchema= new mongoose.Schema({
   name:{ type: String, required: true},
   email:{type: String, required: true},
   password:{type: String, required: true}
+});
+
+UserSchema.pre('save', function(next){
+  const user = this;
+
+  if(this.isModified('password') ||this.isNew){
+    bcrypt.genSalt(10, function(saltError, salt){
+      if (saltError){
+        return next(saltError);
+      } else {
+        bcrypt.hash(user.password, sale, function(hashError, hash) {
+          if (hashError){
+            return next(hashError);
+          }
+          user.password= hash;
+          next();
+        });
+      }
+
+    });
+  } else {
+    return next();
+  }
 });
 
 module.exports=User=mongoose.model('user', UserSchema);
