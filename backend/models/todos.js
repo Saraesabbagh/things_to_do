3219619c3a5cc06c69c03afe0d1d84463045d470
Sarasabbagh <42205895+Sarasabbagh@users.noreply.github.com>
2022-10-31 13:26:00 +0000

    added users to backend database model

--- backend/models/todos.js
+++ backend/models/todos.js
@@ -1,6 +1,7 @@
 const mongoose= require('mongoose');
 
 const TodoSchema= new mongoose.Schema({
+  user_id:{type: String, required:true },
   title:{ type: String, required: true},
   description:{type: String, required: true},
   done:{type: Boolean, required: true}
